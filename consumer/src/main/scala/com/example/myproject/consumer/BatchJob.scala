f1815910c60362dbe8803dd4b3018a534d334d1c
NatAndr <natalia.andrianova@gmail.com>
2021-06-25 00:17:04 +0300

    WIP: batch job

--- consumer/src/main/scala/com/example/myproject/consumer/BatchJob.scala
+++ consumer/src/main/scala/com/example/myproject/consumer/BatchJob.scala
@@ -1,39 +1,61 @@
 package com.example.myproject.consumer
 
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import pureconfig.generic.auto._
+
+import java.time.Duration
+import scala.collection.JavaConverters.{asJavaCollectionConverter, iterableAsScalaIterableConverter}
+
 
 object BatchJob extends App with LazyLogging {
   val config: AppConfig = pureconfig.loadConfigOrThrow[AppConfig]
 
   val spark: SparkSession = SparkSession
     .builder()
-    .appName("Demo")
+    .appName(config.batchConsumerAppName)
     .config("spark.master", "local")
     .getOrCreate()
 
+  spark.sparkContext.setLogLevel("WARN")
+
   import spark.implicits._
 
-  private val df: DataFrame = spark.read
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .csv("provider/data/RUvideos.csv")
+  val consumer: KafkaConsumer[String, String] = KafkaBatchUtils.getConsumer
 
-  df.show()
+  consumer.subscribe(List(config.topic).asJavaCollection)
 
-  val topTags: RDD[(String, Int)] = df.as[YouTubeRecord]
-    .rdd
+  consumer
+    .poll(Duration.ofSeconds(100))
+    .asScala
+    .map(r => r.value())
+    .map(_.replace("\"", "").split(","))
+    .map(YouTubeRecord(_))
     .filter(r => r.tags.nonEmpty)
     .filter(r => r.tags != "[none]")
     .flatMap(r => r.tags.split("\\|"))
     .map(tag => tag.mkString.stripPrefix("\"").stripSuffix("\""))
     .map(tag => tag.toLowerCase())
     .map(tag => (tag, 1))
-    .reduceByKey(_ + _)
-    .sortBy(-_._2)
+//    .
+//    .sortBy(-_._2)
 
-  topTags.take(15).foreach(println)
+//  df.show()
+//
+//  val topTags: RDD[(String, Int)] = df.as[YouTubeRecord]
+//    .rdd
+//    .filter(r => r.tags.nonEmpty)
+//    .filter(r => r.tags != "[none]")
+//    .flatMap(r => r.tags.split("\\|"))
+//    .map(tag => tag.mkString.stripPrefix("\"").stripSuffix("\""))
+//    .map(tag => tag.toLowerCase())
+//    .map(tag => (tag, 1))
+//    .reduceByKey(_ + _)
+//    .sortBy(-_._2)
+//
+//  topTags.take(15).foreach(println)
 
   spark.close()
 
