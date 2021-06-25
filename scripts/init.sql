95a88bf7c53d1f81a890c077642fd838b583a30d
NatAndr <natalia.andrianova@gmail.com>
2021-06-25 16:21:10 +0300

    create youtube_records

--- scripts/init.sql
+++ scripts/init.sql
@@ -24,6 +24,27 @@ CREATE SCHEMA batch
         created_at timestamp with time zone
     );
 
+    CREATE TABLE if not exists youtube_records
+    (
+        video_id varchar,
+        trending_date varchar,
+        title varchar,
+        channel_title varchar,
+        category_id varchar,
+        publish_time varchar,
+        tags varchar,
+        views int,
+        likes int,
+        dislikes int,
+        comment_count int,
+        thumbnail_link varchar,
+        comments_disabled varchar,
+        ratings_disabled varchar,
+        video_error_or_removed varchar,
+        description varchar,
+        created_at timestamp with time zone
+    );
+
 alter table stream.top_tags
     owner to postgres;
 
